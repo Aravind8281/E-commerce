cb1e4b07c765ee40ae2d56547b7a11b22b1c64d9
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-02-16 21:08:37 +0530

    Add files via upload

---
+++ Templates/About.php
@@ -0,0 +1,44 @@
+<section id="about" class="about py-3">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title py-3">
+          <h2>About</h2>
+          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4">
+            <img src="./Assets/1.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-8 pt-4 pt-lg-0 content">
+            <h3>Illustrator &amp; UI/UX Designer</h3>
+            <p class="fst-italic">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua.
+            </p>
+            <div class="row">
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-rounded-right"></i> <strong>Birthday:</strong> 5 Nov 2002</li>
+                  <li><i class="bi bi-rounded-right"></i> <strong>Website:</strong>www.example.com</li>
+                  <li><i class="bi bi-rounded-right"></i> <strong>Phone:</strong>+91 6380558734</li>
+                  <li><i class="bi bi-rounded-right"></i> <strong>City:</strong> Salem,India</li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-rounded-right"></i> <strong>Age:</strong> 20</li>
+                  <li><i class="bi bi-rounded-right"></i> <strong>Degree:</strong> Bachelor</li>
+                  <li><i class="bi bi-rounded-right"></i> <strong>PhEmailone:</strong>v.t.aravindan72@gmail.com</li>
+                  <li><i class="bi bi-rounded-right"></i> <strong>Freelance:</strong> Available</li>
+                </ul>
+              </div>
+            </div>
+            <p class="py-3">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid beatae aliquam rerum dolores molestias reprehenderit doloribus. Minus, qui. Sequi sed provident est, veniam sit neque quia sint non facilis libero!
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section>
---
+++ Templates/Accounts.php
@@ -0,0 +1,71 @@
+<section style="background-color: #eee;">
+  <div class="container py-5">
+  
+    <div class="row">
+      <div class="col-lg-4">
+        <div class="card mb-4">
+          <div class="card-body text-center">
+            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar"
+              class="rounded-circle img-fluid" style="width: 150px;">
+            <h5 class="my-3">Aravindan</h5>
+            <p class="text-muted mb-1">Full Stack Developer</p>
+            <div class="d-flex justify-content-center mb-2">
+              <button type="button" class="btn btn-primary">Follow</button>
+              <button type="button" class="btn btn-outline-primary ms-1">Message</button>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+      <div class="col-lg-8">
+        <div class="card mb-4">
+          <div class="card-body">
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Full Name</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">Aravindan V</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Email</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">v.t.aravindan72@gmail.com</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Phone</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">6380558734</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Mobile</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">6380558734</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Address</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">Salem Tamil nadu</p>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+</section>
